8bbab2c50ac5d9f5e56b6c05ff281eb9af10679e
Shuqi Liao <373628977@qq.com>
2019-10-27 17:47:17 +0800

    responsive justify

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {Navbar, Jumbotron, Nav, Container, Row, Col, Card, Button} from 'react-bootstrap';
+import {Navbar, Jumbotron, Nav, Container, Row, Col, Card, Button, NavbarCollapse} from 'react-bootstrap';
 import styles from '../stylesheet/Home.css';
 import cac_home_logo from '../assets/cac_home_logo.png';
 import thoughtworks_logo from '../assets/thoughtworks_logo.png'
@@ -13,7 +13,7 @@ class Home extends Component {
     render() {
       return(
         <div className="home">
-          <Navbar bg="transparents" variant="dark" Style="border-radius:0">
+          <Navbar collapseOnSelect expand="lg" bg="transparents" variant="dark" Style="border-radius:0">
             <Navbar.Brand href="#home">
               <img
                 alt=""
@@ -24,16 +24,20 @@ class Home extends Component {
               />{'  '}
               <span Style="font-size:20px"><strong>C</strong>hina <strong>A</strong>gile <strong>C</strong>oach</span>
             </Navbar.Brand>
-            <Nav className="ml-auto nav-link">
-              <Nav.Link href="#home">首页</Nav.Link>
-              <Nav.Link href="#features">课程体系</Nav.Link>
-              <Nav.Link href="#pricing">认证体系</Nav.Link>
-              <Nav.Link href="#pricing">认证讲师</Nav.Link>
-              <Nav.Link href="#pricing">证书查询</Nav.Link>
-              <Nav.Link href="#pricing">CAC动态</Nav.Link>
-              <Nav.Link href="#pricing">加入我们</Nav.Link>
-            </Nav>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto nav-link">
+                <Nav.Link href="#home">首页</Nav.Link>
+                <Nav.Link href="#features">课程体系</Nav.Link>
+                <Nav.Link href="#pricing">认证体系</Nav.Link>
+                <Nav.Link href="#pricing">认证讲师</Nav.Link>
+                <Nav.Link href="#pricing">证书查询</Nav.Link>
+                <Nav.Link href="#pricing">CAC动态</Nav.Link>
+                <Nav.Link href="#pricing">加入我们</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
+
           <Jumbotron fluid>
             <Container className="home-jumbotron">
               <img src={cac_home_logo} className="home-logo-img"/>
@@ -58,8 +62,8 @@ class Home extends Component {
               <Col Style="color:#000000;text-align:center;font-size:10px">COURSE MODEL</Col>
               </Row>
               <Row className="course-model-content">
-                <Col lg="4" Style="color:#000000;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Col>
-                <Col lg="8" Style="text-align:right;">
+                <Col lg="4" md="12" sm="12" Style="color:#000000;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Col>
+                <Col lg="8" md="12" sm="12" className="course-model-img">
                   <img src={training_model} Style="width:55%"/>
                 </Col>
               </Row>
@@ -72,17 +76,17 @@ class Home extends Component {
                 <Col Style="color:#000000;text-align:center;font-size:10px">SUB-BRAND</Col>
               </Row>
               <Row className = "sub-brand-content">
-                <Col md lg="3" className="  sub-brand-col">
+                <Col lg="3" md="12" sm="12" xs="12" className="sub-brand-col">
                   <img src={sub_brand_green} width="150px"/>
                   <div Style="margin:10px 0; font-size:15px;">面向企业，塑造「仪式感」</div>
                   <div>xxxxxxxxxxxxxxxxxxxxxxxxx</div>
                 </Col>
-                <Col md lg="3" className="sub-brand-col">
+                <Col lg="3" md="12" sm="12" xs="12" className="sub-brand-col">
                   <img src={sub_brand_blue} width="150px"/>
                   <div Style="margin:10px 0; font-size:15px;">面向课堂，塑造「方向感」</div>
                   <div>xxxxxxxxxxxxxxxxxxxxxxxxx</div>
                 </Col>
-                <Col md lg="3" className="sub-brand-col">
+                <Col lg="3" md="12" sm="12" xs="12" className="sub-brand-col">
                   <img src={sub_brand_purple} width="150px"/>
                   <div Style="margin:10px 0; font-size:15px;">面向社区，塑造「归属感」</div>
                   <div>xxxxxxxxxxxxxxxxxxxxxxxxx</div>
